c15704d29ea1a58e8af4d2cc18706e10b6695659
Sifu Biri <sifeddinebiri@gmail.com>
2017-01-12 18:57:06 -0600

    solved exercices

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,69 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn boolean
+  [x]
+  (if x
+    true
+    false))
 
-(defn abs [x]
-  ":(")
+(defn teen?
+  [x]
+  (>= 19 x 13))
 
-(defn divides? [divisor n]
-  ":(")
 
-(defn fizzbuzz [n]
-  ":(")
+(defn abs
+  [x]
+  (if (neg? x)
+    (- x)
+    x))
 
-(defn teen? [age]
-  ":(")
+(defn divides?
+  [divisor n]
+  (= (mod n divisor) 0))
+
+
+(defn fizzbuzz
+  [x]
+  (cond
+    (divides? 15 x) "gotcha!"
+    (divides? 3 x) "fizz"
+    (divides? 5 x) "buzz"
+    :else ""))
+
+
+(defn fizzbuzz2
+  [x]
+  (condp  divides? x
+    15  "gotcha"
+    3 "fizz"
+    5 "buzz"
+    ""))
+
+
+(defn teen?
+  [x]
+  (>= 19 x 13))
+
 
 (defn not-teen? [age]
-  ":(")
+  (complement teen?))
+
+
+(defn not-teen? [age]
+  (not (>= 19 age 13)))
+
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* x 2)
+    (empty? x) nil
+    (or (vector? x) (list? x)) (* (count x) 2)
+    :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (if (>= year 100)
+    (divides? 400 year)
+    (divides? 4 year)))
 
 ; '_______'
